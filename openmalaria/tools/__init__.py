775562f5633439289d3fc5e7ad45eb72c2f2bed8
Alex Vyushkov <alexander.vyushkov[at]nd.edu>
2015-02-11 09:30:20 -0500

    Updated license information

--- openmalaria/tools/__init__.py
+++ openmalaria/tools/__init__.py
@@ -1,4 +1,3 @@
-#
 # This file is part of the openmalaria.tools package.
 # For copyright and licensing information about this package, see the
 # NOTICE.txt and LICENSE.txt files in its top-level directory; they are
@@ -7,4 +6,3 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License (MPL), version 2.0.  If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
--- openmalaria/tools/plotResult.py
+++ openmalaria/tools/plotResult.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-This file is part of OpenMalaria.
-
-Copyright (C) 2005-2014 Swiss Tropical Institute
-
-OpenMalaria is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-"""
+#
+# This file is part of the openmalaria.tools package.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/openmalaria.tools
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from optparse import OptionParser
 import sys
